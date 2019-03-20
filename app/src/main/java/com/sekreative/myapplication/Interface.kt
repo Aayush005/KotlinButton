b74a45aaa5ac0dff743afbcb75b22073eb376ed5
Aayush005 <aayush.upadhyay9@gmail.com>
2019-03-21 01:02:02 +0530

    updated blog

---
+++ app/src/main/java/com/sekreative/myapplication/Interface.kt
@@ -0,0 +1,9 @@
+package com.sekreative.myapplication
+
+
+interface Interface  {
+    var myVar: String            // abstract property
+
+    fun hello() {
+    }
+}
--- app/src/main/java/com/sekreative/myapplication/MainActivity.kt
+++ app/src/main/java/com/sekreative/myapplication/MainActivity.kt
@@ -4,12 +4,21 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Interface{
+
+    override var myVar: String = "Worked!"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         updateButton.setOnClickListener { welcomeTextView.text = "Kotlin is so Easy!" }
+
+        hello()
+    }
+
+    override fun hello() {
+        updateButton2.setOnClickListener { welcomeTextView.text = "Interface " + myVar}
     }
 }
+
